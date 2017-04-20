a9fcf412a040b87f5007812322fe4463b048db41
Christian Fobel <christian@fobel.net>
2017-04-20 05:49:08 -0400

    Add versioneer files

--- __init__.py
+++ __init__.py
@@ -134,3 +134,7 @@ class StepLabelPlugin(Plugin, StepOptionsController):
 
 
 PluginGlobals.pop_env()
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
